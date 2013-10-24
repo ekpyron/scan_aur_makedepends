d83ea5af23fea3727be8b04a0460ac1ad15e5071
Daniel Kirchner <daniel@ekpyron.org>
2013-10-24 18:08:17 +0200

    Initial commit

---
+++ scan_aur_makedepends.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+PKGNAMES=$(package-query -SAs -f "%n" "$1")
+for PKGNAME in ${PKGNAMES}; do
+{
+wget -q -O - https://aur.archlinux.org/packages/${PKGNAME:0:2}/${PKGNAME}/PKGBUILD;
+echo
+echo "for dep in \"\${makedepends[@]}\"; do if [ \"\$dep\" == \"$2\" ]; then echo $PKGNAME; fi; done";
+} | bash
+#echo $makedepends
+done
